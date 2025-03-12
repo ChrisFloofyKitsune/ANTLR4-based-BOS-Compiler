d55f3e14745894c389153dad072ddd650c3990a0
ChrisFloofyKitsune <krizasan@gmail.com>
2025-03-12 09:21:19 -0600

    move ValueDispatch to own file

--- bos_tree_sitter/ts_ast_visitor.py
+++ bos_tree_sitter/ts_ast_visitor.py
@@ -1,50 +1,13 @@
 import operator
 import warnings
-from functools import singledispatchmethod, update_wrapper, reduce
+from functools import singledispatchmethod, reduce
 from typing import Any, ClassVar
 
 import tree_sitter
 import tree_sitter_bos
 
 from bos import ast_nodes
-
-
-class ValueDispatch:
-    """ A single-dispatch that can be used as a decorator/descriptor. """
-    def __init__(self, func):
-        if not callable(func) and not hasattr(func, "__get__"):
-            raise TypeError(f"{func!r} is not callable or a descriptor")
-
-        self.dispatch_table = {}
-        self.func = func
-
-    def register(self, value):
-        def decorator(func):
-            if not callable(func) and not hasattr(func, "__get__"):
-                raise TypeError(f"{func!r} is not callable or a descriptor")
-
-            self.dispatch_table[value] = func
-            return func
-
-        return decorator
-
-    def __get__(self, obj, cls=None):
-        def _do_call(call, *args, **kwargs):
-            if hasattr(call, '__get__'):
-                return call.__get__(obj, cls)(*args, **kwargs)
-            return call(*args, **kwargs)
-
-        def _method(value, *args, **kwargs):
-            method = self.dispatch_table.get(value, None)
-            if method is None:
-                return _do_call(self.func, value, *args, **kwargs)
-            return _do_call(method, *args, **kwargs)
-
-        _method.register = self.register
-        _method.dispatch_table = self.dispatch_table
-        update_wrapper(_method, self.func)
-        return _method
-
+from value_dispatch import ValueDispatch
 
 _bos_lang = tree_sitter.Language(tree_sitter_bos.language())
 
